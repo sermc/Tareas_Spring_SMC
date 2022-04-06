51f3c1d8fc09074673974fd6dd7c87a9f0d848c7
smoracar <smoracar@emeal.nttdata.com>
2022-04-06 11:04:04 +0200

    content

--- nttdatacenters-spring-t5-SMC/src/main/java/com/sergio/nttdatacentersspringt5SMC/controllers/ClienteController.java
+++ nttdatacenters-spring-t5-SMC/src/main/java/com/sergio/nttdatacentersspringt5SMC/controllers/ClienteController.java
@@ -3,6 +3,7 @@ package com.sergio.nttdatacentersspringt5SMC.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,29 +15,37 @@ import org.springframework.web.bind.annotation.RestController;
 import com.sergio.nttdatacentersspringt5SMC.entity.Cliente;
 import com.sergio.nttdatacentersspringt5SMC.service.ClienteService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
+@Api(value="clientesapi")
 public class ClienteController {
 	
 	@Autowired
 	ClienteService servicio;
 	
+	@ApiOperation(value = "Listado todos los clientes", response = Cliente.class)
 	@RequestMapping(value="listar", method = RequestMethod.GET,produces = "application/json")
-	public List<Cliente> getClientes(Model modelAndView){
-		return servicio.buscarTodos();
+	public ResponseEntity<List<Cliente>> getClientes(Model modelAndView){
+		return ResponseEntity.ok(servicio.buscarTodos());
 	}
 	
+	@ApiOperation(value = "Añadir un cliente", response = Cliente.class)
 	@PostMapping("insert")
-	public Cliente insertCliente(@RequestBody Cliente c,Model modelAndView) {		
-		return servicio.insert(c);
+	public ResponseEntity<Cliente> insertCliente(@RequestBody Cliente c,Model modelAndView) {		
+		return ResponseEntity.ok(servicio.insert(c));
 	}
 	
+	@ApiOperation(value = "Eliminar un cliente", response = Cliente.class)
 	@RequestMapping(value="delete/{id}", method = RequestMethod.DELETE, produces = "application/json")
-	public String deleteCliente(@PathVariable Long id,Model modelAndView) {
-		return servicio.delete(id);
+	public ResponseEntity<String> deleteCliente(@PathVariable Long id,Model modelAndView) {
+		return ResponseEntity.ok(servicio.delete(id));
 	}
 	
+	@ApiOperation(value = "Filtrar clientes por nombre", response = Cliente.class)
 	@RequestMapping(value="searchByName/{nombre}", method = RequestMethod.GET,produces = "application/json")
-	public List<Cliente> getClientesByName(@PathVariable("nombre") String nombre){
-		return servicio.findByNombreIgnoreCaseLike(nombre);
+	public ResponseEntity<List<Cliente>> getClientesByName(@PathVariable("nombre") String nombre){
+		return ResponseEntity.ok(servicio.findByNombreIgnoreCaseLike(nombre));
 	}
 }
--- nttdatacenters-spring-t5-SMC/src/main/java/com/sergio/nttdatacentersspringt5SMC/entity/Cliente.java
+++ nttdatacenters-spring-t5-SMC/src/main/java/com/sergio/nttdatacentersspringt5SMC/entity/Cliente.java
@@ -1,8 +1,18 @@
 package com.sergio.nttdatacentersspringt5SMC.entity;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
 @Table(name="CLIENTE")
 public class Cliente {
 	@Id
@@ -11,65 +21,21 @@ public class Cliente {
 	
 	@Column(name="NOMBRE",nullable=false)
 	private String nombre;
-	
+	  
 	@Column(name="APELLIDOS",nullable=false)
 	private String apellidos;
 	
 	@Column(name="FECHA_NACIMIENTO",nullable=false)
 	private String fechaNacimiento;
-	
+	 
 	@Column(name="dni",unique=true,nullable=false,length=9)
 	private String dni;
-
-	public Cliente() {
-		super();
-	}
 
 	public Cliente(String nombre, String apellidos, String fechaNacimiento, String dni) {
 		super();
 		this.nombre = nombre;
 		this.apellidos = apellidos;
 		this.fechaNacimiento = fechaNacimiento;
-		this.dni = dni;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getApellidos() {
-		return apellidos;
-	}
-
-	public void setApellidos(String apellidos) {
-		this.apellidos = apellidos;
-	}
-
-	public String getFechaNacimiento() {
-		return fechaNacimiento;
-	}
-
-	public void setFechaNacimiento(String fechaNacimiento) {
-		this.fechaNacimiento = fechaNacimiento;
-	}
-
-	public String getDni() {
-		return dni;
-	}
-
-	public void setDni(String dni) {
 		this.dni = dni;
 	}
 	
